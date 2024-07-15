bdb958b1941bc8fe8da2f444a0f510bb05a38cda
Andrey_Khobotov <and0rscg@gmail.com>
2024-07-15 21:25:21 +0300

    add step 4

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/And0rs/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/And0rs/frontend-project-46/actions)
+
+<h2 style='text-align: center'> Preview </h2>
+<ul>
+<li><a href="https://asciinema.org/a/645795">Brain even</a></li>
+</ul>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import path from 'node:path';
 import process from 'node:process';
 import fs from 'node:fs';
+import _ from 'lodash';
 
 const getPath = (filePath) => path.resolve(process.cwd(), filePath);
 
@@ -8,13 +9,40 @@ const getFile = (filePath) => fs.readFileSync(filePath);
 
 const parse = (filePath) => JSON.parse(getFile(filePath));
 
+const getDiff = (obj1, obj2) => {
+  // Получаем массив уникальных отсортированных ключей
+  // Хотел использовать uniq, но он принимает только один массив, пришлось бы тогда дополнительно объединять массивы
+  const sortedKeys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
+  const result = {};
+
+  // Получаем результат сравнения двух объектов
+  // если не 2, если не 1, если равны, остальное (если не равны)
+  for (const key of sortedKeys) {
+    if (!Object.hasOwn(obj2, key)) {
+      result[`- ${key}`] = obj1[key];
+    } else if (!Object.hasOwn(obj1, key)) {
+      result[`+ ${key}`] = obj2[key];
+    } else if (obj1[key] === obj2[key]) {
+      result[key] = obj1[key]
+    } else {
+      result[`- ${key}`] = obj1[key];
+      result[`+ ${key}`] = obj2[key];
+    }
+  }
+  return result;
+};
+
 const gendiff = (filePath1, filePath2) => {
   const path1 = getPath(filePath1);
   const path2 = getPath(filePath2);
 
-  const file1 = parse(path1);
-  const file2 = parse(path2);
-  console.log(file1, file2);
+  const obj1 = parse(path1);
+  const obj2 = parse(path2);
+  // console.log(obj1);
+  // console.log(obj2);
+
+  const result = getDiff(obj1, obj2);
+  console.log(result);
 };
 
 export default gendiff;
