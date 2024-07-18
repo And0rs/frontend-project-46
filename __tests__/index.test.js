9afd44c891d06621ece339811a34c911f3817d1a
Andrey_Khobotov <and0rscg@gmail.com>
2024-07-18 18:50:54 +0300

    update tests, remove duplicates

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,62 +1,44 @@
 import gendiff from '../src/index.js';
 import getFixturePath from '../src/fixturePath.js';
 
-test('flat diff with two JSON', () => {
-  const expectedObj = {
-    '- follow': false,
-    host: 'hexlet.io',
-    '- proxy': '123.234.53.22',
-    '- timeout': 50,
-    '+ timeout': 20,
-    '+ verbose': true,
-  };
+const expectedObj1 = {
+  '- follow': false,
+  host: 'hexlet.io',
+  '- proxy': '123.234.53.22',
+  '- timeout': 50,
+  '+ timeout': 20,
+  '+ verbose': true,
+};
 
+const expectedObj2 = {
+  '+ follow': false,
+  host: 'hexlet.io',
+  '+ proxy': '123.234.53.22',
+  '- timeout': 20,
+  '+ timeout': 50,
+  '- verbose': true,
+};
+
+test('flat diff with two JSON', () => {
   expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json')))
-    .toEqual(expectedObj);
+    .toEqual(expectedObj1);
 });
 
 test('flat diff with two yml', () => {
-  const expectedObj = {
-    '- follow': false,
-    host: 'hexlet.io',
-    '- proxy': '123.234.53.22',
-    '- timeout': 50,
-    '+ timeout': 20,
-    '+ verbose': true,
-  };
-
   expect(gendiff(getFixturePath('file1.yml'), getFixturePath('file2.yml')))
-    .toEqual(expectedObj);
+    .toEqual(expectedObj1);
 });
 
 test('flat diff with different formats', () => {
-  const expectedObj = {
-    '- follow': false,
-    host: 'hexlet.io',
-    '- proxy': '123.234.53.22',
-    '- timeout': 50,
-    '+ timeout': 20,
-    '+ verbose': true,
-  };
-
   expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.yml')))
-    .toEqual(expectedObj);
+    .toEqual(expectedObj1);
   expect(gendiff(getFixturePath('file1.yml'), getFixturePath('file2.json')))
-    .toEqual(expectedObj);
+    .toEqual(expectedObj)1;
 });
 
 test('flat diff with different formats, switch files', () => {
-  const expectedObj = {
-    '+ follow': false,
-    host: 'hexlet.io',
-    '+ proxy': '123.234.53.22',
-    '- timeout': 20,
-    '+ timeout': 50,
-    '- verbose': true,
-  };
-
   expect(gendiff(getFixturePath('file2.json'), getFixturePath('file1.yml')))
-    .toEqual(expectedObj);
+    .toEqual(expectedObj2);
   expect(gendiff(getFixturePath('file2.yml'), getFixturePath('file1.json')))
-    .toEqual(expectedObj);
+    .toEqual(expectedObj2);
 });
